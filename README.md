a4613aef3a3a1ec1ab4fdedcd308d7edd37f8d8f
chenvv.cw <chenvv.cw@gmail.com>
2022-07-20 11:17:00 +0800

    init auncel

---
+++ README.md
@@ -0,0 +1,138 @@
+# Auncel (A Learned Query Optimizer by Ranking Plans)
+Query optimizer (QO) is the core part, as well as the most challenging problem, in DBMS. 
+We witness that the relative order (or rank) of plans actually matters to QO. Learning the rough rank scores is much easier than the unique latency value. To this end, we design Auncel, a new learned QO system following the rank-based paradigm.  
+And this repository is a naive demo of Auncel based on PostgreSQL. In the future, we will release a development framework that facilitates the integration of machine learning algorithms on the database while avoiding direct modifications to the source code, and then formally do the code refactoring of this project on it. 
+
+---
+## Setup
+
+This demo is based on a modified version of PostgreSQL 13.1, these are some related installation processes.
+
+```bash
+# 1. download the PostgreSQL 13.1  
+wget https://ftp.postgresql.org/pub/source/v13.1/postgresql-13.1.tar.bz2
+tar -xvf postgresql-13.1.tar.bz2
+
+# 2. apply some modifications on it
+cd postgresql-13.1
+git apply ../0001-init-auncel.patch
+
+# 3. install PostgreSQL
+make
+make install
+
+# 4. modify the configuration of PostgreSQL in postgresql.conf
+listen_addresses = '*'
+geqo = off
+max_parallel_workers = 0
+max_parallel_workers_per_gather = 0
+```
+---
+
+## Run the Demo
+In this demo, we use an independent server to simulate most of the features of Auncel.
+Auncel is mainly composed of two stages:   
+
+1. generate different execution plans according to different policies  
+2. use a model to select an optimal execution plan
+
+For the convenience of demonstration, these two parts are put in the server. PostgreSQL completes the corresponding work by communicating with the server.
+
+
+### Start Server
+```bash
+python server.py
+```
+The port and host of the server are configured in the server.conf. 
+PostgreSQL uses the same preset port and host to communicate with the server.
+If you want to modify these two configurations, you need to execute two additional commands in PSQL every time you execute a query.
+```bash
+SET auncel_server_host TO "new_host";
+SET auncel_server_port TO new_port;
+```
+
+
+### Collect Plans and Re-train Model
+Here we use TPC-H queries in 1GB data set for demonstration (You can refer to this work ([tpch-dbgen](https://github.com/electrum/tpch-dbgen)) to create corresponding data set).  
+Relevant scripts are placed in "./test_script". 
+**And please modify the configurations in "config.py" before execution to ensure the script can correctly connect to the server and control the database.**
+
+Use the following command to start demo. 
+This script will load the training queries and test queries, and the model will be retrained every 'query_num_per_chunk' queries. Auncel will collect performance on the test queries after each training phase.
+
+```bash
+# output_query_latency_file: the final executed plan will be output to this file
+# model_prefix: prefix of model name
+# topK: the number of plans that can be explored by each query
+python train_model.py --query_path tpch_train.txt --test_query_path tpch_test.txt --algo auncel --query_num_per_chunk 20 --output_query_latency_file auncel_tpch.log --model_prefix tpch_test_model --topK 3
+```
+Four kinds of files will be generated gradually during the execution of this script:
+1. auncel_tpch.log  
+    The best plan considered by model in Auncel will be executed and the results will be output to this file.
+2. auncel_tpch.log_exploratory  
+    Other plans for pairwise training of each query will be executed and output to this file.
+3. auncel_tpch.log.training  
+    Integrate the results of "auncel_tpch.log" and "auncel_tpch.log_exploratory" for model training.
+4. auncel_tpch.log_tpch_test_model_i  
+    The performance of the model after i-th training.
+
+In order to compare the results, after Auncel executes all the queries, we use PostgreSQL to execute them again.
+The plans of the training set and the test set will be saved in "pg_tpch_train.log" and "pg_tpch_test.log" respectively.
+```bash
+python train_model.py --query_path tpch_train.txt  --algo pg --output_query_latency_file pg_tpch_train.log
+python train_model.py --query_path tpch_test.txt --algo pg --output_query_latency_file pg_tpch_test.log
+```
+
+### Result Visualization
+Here we can use jupyter to easily visualize the results of the experiment (see visualization.ipynb for details).  
+In the training set, Auncel will gradually surpass PostgreSQL after the first training.
+And in the test set, Auncel runs 1.2x faster than PostgreSQL.
+<center class="half">
+    <img src="auncel/test_script/train.jpg" width="200"/>
+    <img src="auncel/test_script/test.jpg" width="200"/>
+</center>
+
+
+<!-- 
+---
+## Paper Citation
+```bash
+TODO
+```
+-->
+
+---
+## Re-produce
+We also put some files here to help you reproduce the result of some experiments in paper. You can fine them in "auncel/reproduce".  
+
+1. Create database  
+We dump the data of STATS to facilitate you to rebuild the database. (You can rebuild JOB by [join-order-benchmark](https://github.com/gregrahn/join-order-benchmark))
+```bash
+psql > CREATE DATABASE stats;
+psql -d stats -f stats_db.sql
+```
+
+2. Load Model  
+There are three trained models (imdb_pw, stats_pw and tpch_pw) available in the folder. You can modify the configuration file "server.conf" to load the corresponding model. We choose to load "stats_pw" here.
+```bash
+# in server.conf
+ModelPath = ./reproduce/stats_pw
+```
+
+3. Test the effect of the model  
+As in the previous example, we first need to start the server and set the corresponding script configuration.
+And then execute the script to collect the results of the model on the test set.
+```bash
+# start server
+python server.py
+# in conf.py
+...
+DB = stats
+...
+# execute the script
+python test.py --query_path ../reproduce/test/stats.txt --output_query_latency_file stats.test
+```
+
+
+---
+Thanks for the implementation of ["Tree Convolution"](https://github.com/RyanMarcus/TreeConvolution).
---
+++ auncel/train.py
@@ -0,0 +1,219 @@
+import argparse
+import math
+
+from feature import *
+from model import AuncelModel, AuncelModelPairWise
+
+
+def _load_pointwise_plans(path):
+    with open(path, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def _load_pairwise_plans(path):
+    X1, X2 = [], []
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            arr = line.split("#####")
+            x1, x2 = get_training_pair(arr)
+            X1 += x1
+            X2 += x2
+    return X1, X2
+
+
+def get_training_pair(candidates):
+    assert len(candidates) >= 2
+    X1, X2 = [], []
+
+    i = 0
+    while i < len(candidates) - 1:
+        s1 = candidates[i]
+        j = i + 1
+        while j < len(candidates):
+            s2 = candidates[j]
+            X1.append(s1)
+            X2.append(s2)
+            j += 1
+        i += 1
+    return X1, X2
+
+
+def compute_rank_score(path, pretrain=False, rank_score_type=0):
+    X, Y = [], []
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            arr = line.split("#####")
+            if pretrain:
+                arr = [(json.loads(p)[0]['Plan']['Total Cost'], p)
+                       for p in arr]
+            else:
+                arr = [(json.loads(p)[0]['Execution Time'], p) for p in arr]
+            sorted_arr = sorted(arr, key=lambda x: x[0])
+
+            for i in range(len(sorted_arr)):
+                X.append(sorted_arr[i][1])
+                if rank_score_type == 0:
+                    # 1. x^2
+                    print("X^2")
+                    Y.append(float((i + 1) ** 2))
+                elif rank_score_type == 1:
+                    # 2. x^4
+                    print("X^4")
+                    Y.append(float((i + 1) ** 4))
+                elif rank_score_type == 2:
+                    # 3. e^x
+                    print("e^X")
+                    Y.append(float(math.exp(i+1)))
+                elif rank_score_type == 3:
+                    # 3. x^1
+                    print("X^1")
+                    Y.append(float((i + 1)))
+    return X, Y
+
+
+def training_pairwise(tuning_model_path, model_name, training_data_file, pretrain=False):
+    X1, X2 = _load_pairwise_plans(training_data_file)
+
+    tuning_model = tuning_model_path is not None
+    auncel_model = None
+    if tuning_model:
+        auncel_model = AuncelModelPairWise(None)
+        auncel_model.load(tuning_model_path)
+        feature_generator = auncel_model._feature_generator
+    else:
+        feature_generator = FeatureGenerator()
+        feature_generator.fit(X1 + X2)
+
+    Y1, Y2 = None, None
+    if pretrain:
+        Y1 = [json.loads(c)[0]['Plan']['Total Cost'] for c in X1]
+        Y2 = [json.loads(c)[0]['Plan']['Total Cost'] for c in X2]
+        X1, _ = feature_generator.transform(X1)
+        X2, _ = feature_generator.transform(X2)
+    else:
+        X1, Y1 = feature_generator.transform(X1)
+        X2, Y2 = feature_generator.transform(X2)
+    print("Training data set size = " + str(len(X1)))
+
+    if not tuning_model:
+        assert auncel_model == None
+        auncel_model = AuncelModelPairWise(feature_generator)
+    auncel_model.fit(X1, X2, Y1, Y2, tuning_model)
+
+    print("saving model...")
+    auncel_model.save(model_name)
+
+
+def training_with_rank_score(tuning_model_path, model_name, training_data_file, pretrain=False, rank_score_type=0):
+    X, Y = compute_rank_score(training_data_file, pretrain, rank_score_type)
+
+    tuning_model = tuning_model_path is not None
+    auncel_model = None
+    if tuning_model:
+        auncel_model = AuncelModel(None)
+        auncel_model.load(tuning_model_path)
+        feature_generator = auncel_model._feature_generator
+    else:
+        feature_generator = FeatureGenerator()
+        feature_generator.fit(X)
+
+    # replace lantency with rank score
+    local_features, _ = feature_generator.transform(X)
+    assert len(local_features) == len(Y)
+    print("Training data set size = " + str(len(local_features)))
+
+    if not tuning_model:
+        assert auncel_model == None
+        auncel_model = AuncelModel(feature_generator)
+
+    auncel_model.fit(local_features, Y, tuning_model)
+
+    print("saving model...")
+    auncel_model.save(model_name)
+
+
+def training_pointwise(tuning_model_path, model_name, training_data_file):
+    X = _load_pointwise_plans(training_data_file)
+
+    tuning_model = tuning_model_path is not None
+    auncel_model = None
+    if tuning_model:
+        auncel_model = AuncelModel(None)
+        auncel_model.load(tuning_model_path)
+        feature_generator = auncel_model._feature_generator
+    else:
+        feature_generator = FeatureGenerator()
+        feature_generator.fit(X)
+
+    local_features, y = feature_generator.transform(X)
+    assert len(local_features) == len(y)
+    print("Training data set size = " + str(len(local_features)))
+
+    if not tuning_model:
+        assert auncel_model == None
+        auncel_model = AuncelModel(feature_generator)
+
+    auncel_model.fit(local_features, y, tuning_model)
+
+    print("saving model...")
+    auncel_model.save(model_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Model training helper")
+    parser.add_argument("--training_data",
+                        metavar="PATH",
+                        help="Load the queries")
+    parser.add_argument("--training_type", type=int)
+    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--pretrain_model_name", type=str)
+    parser.add_argument("--rank_score_training_type", type=int)
+
+    args = parser.parse_args()
+
+    training_type = 0
+    if args.training_type is not None:
+        training_type = args.training_type
+    print("training_type:", training_type)
+
+    training_data = None
+    if args.training_data is not None:
+        training_data = args.training_data
+    print("training_data:", training_data)
+
+    model_name = None
+    if args.model_name is not None:
+        model_name = args.model_name
+    print("model_name:", model_name)
+
+    pretrain_model_name = None
+    if args.pretrain_model_name is not None:
+        pretrain_model_name = args.pretrain_model_name
+    print("pretrain_model_name:", pretrain_model_name)
+
+    rank_score_training_type = 0
+    if args.rank_score_training_type is not None:
+        rank_score_training_type = args.rank_score_training_type
+    print("rank_score_training_type:", rank_score_training_type)
+
+    if training_type == 0:
+        print("training_pointwise")
+        training_pointwise(pretrain_model_name, model_name, training_data)
+    elif training_type == 1:
+        print("training_pairwise")
+        training_pairwise(pretrain_model_name, model_name,
+                          training_data, False)
+    elif training_type == 2:
+        print("training_with_rank_score")
+        training_with_rank_score(
+            pretrain_model_name, model_name, training_data, False, rank_score_training_type)
+    elif training_type == 3:
+        print("pre-training_pairwise")
+        training_pairwise(pretrain_model_name, model_name,
+                          training_data, True)
+    elif training_type == 4:
+        print("pre-training_with_rank_score")
+        training_with_rank_score(
+            pretrain_model_name, model_name, training_data, True, rank_score_training_type)
+    else:
+        raise Exception()
